43d94a0c128ba911184385891195f126e2c55aaf
Rafael Santos de Souza <rafael.souza@globo.com>
2022-12-15 19:34:47 -0300

    Last update

--- pages/views.py
+++ pages/views.py
@@ -8,10 +8,19 @@ def home(request):
     teams = Team.objects.all()
     features_cars = Car.objects.order_by('-created_at').filter(is_featured=True)
     all_cars = Car.objects.order_by('-created_at')
+    model_search = Car.objects.values_list('model', flat=True).distinct()
+    city_search = Car.objects.values_list('city', flat=True).distinct()
+    body_style_search = Car.objects.values_list('body_style', flat=True).distinct()
+    year_search = Car.objects.values_list('year', flat=True).distinct()
+
     data = {
         'teams': teams,
         'featured_cars': features_cars,
         'all_cars': all_cars,
+        'model_search': model_search,
+        'city_search': city_search,
+        'body_style_search': body_style_search,
+        'year_search': year_search,
     }
     return render(request, 'pages/home.html', data)
 
